9d1fd78bdc71a21758e2371c48982f4266415645
nfilin480 <santes060@gmail.com>
2020-06-03 22:19:22 +0300

    fix_2

--- tests/archer_test.h
+++ tests/archer_test.h
@@ -26,7 +26,7 @@ TEST(arrow_of_archer, onDistance){
     for ( float time = 10; time < 100; time+=5){
         p.enemy_archer(time, 0);
     }
-    ASSERT_EQ(p.index_arrows.size(), 0);
+    ASSERT_EQ((int)p.index_arrows.size(), 0);
     ASSERT_EQ(alive, true);
 }
 
