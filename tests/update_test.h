87be64840d99a116c57ee52746c7d1b34ce9bf9c
nfilin480 <santes060@gmail.com>
2020-06-04 10:45:41 +0300

    third test add

--- tests/update_test.h
+++ tests/update_test.h
@@ -29,6 +29,26 @@ TEST(update, jump){
         p.update(time);
     ASSERT_EQ(p.rect.left, 32);
 
+}
+TEST(update, negative){
+    sf::Texture t;
+    t.loadFromFile(SPRITE"/ninja.png");
+
+    sf::Texture a;
+    a.loadFromFile(SPRITE"/archer.png");
+
+    PLAYER p(t, a);
+
+    p.rect.left = 32;
+    p.rect.top = 416;
+
+    p.dx = 0;
+    p.dy = 0.3;
+    p.onGround = false;
+    for (float time = 100; time < 3000; time+=100)
+        p.update(time);
+    ASSERT_EQ(p.rect.left, 32);
+    ASSERT_EQ(p.rect.top, 416);
 
 }
 
